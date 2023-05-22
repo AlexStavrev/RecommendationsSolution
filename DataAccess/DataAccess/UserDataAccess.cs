cbf1d9d867ca6a4dab4f99103a3a7542f8fd2245
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-05-22 13:28:50 +0200

    User class structure and data access interface

---
+++ DataAccess/DataAccess/UserDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Interfaces;
+using DataAccess.Models;
+
+namespace DataAccess.DataAccess;
+internal class UserDataAccess : IUserDataAccess
+{
+    public Task<User> CreateUserAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task LikeMovieAsync(int userId, int movieId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> LoginUserAsync(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SeeMovieAsync(int userId, int movieId)
+    {
+        throw new NotImplementedException();
+    }
+}

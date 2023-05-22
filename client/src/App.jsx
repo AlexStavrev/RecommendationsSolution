181fe36cd4139e647726c072d2f7c941f42a0f0a
AdamGalla <ado.galla@gmail.com>
2023-05-22 13:46:58 +0200

    Landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('');
+  const logIn = (name) => {
+    //Call api
+    console.log(name)
+  }
+
+  const register = (name) => {
+        //Call api
+        console.log(name)
+  }
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h2>Movie recomendation system</h2>
+      <div className='logo'>
+          {/* <img src="public/logo.png" className="logo" alt="Movie logo" /> */}
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <input placeholder="Your name" type="text" value={name} onChange={handleChange} />
+      <div className='buttons'>
+      <button className="button" onClick={logIn(name)}>
+          Log In
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button className="button" onClick={register(name)}>
+          Register
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
+      </div>
     </>
   )
 }

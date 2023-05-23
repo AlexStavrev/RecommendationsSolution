81dd4807ff05ee47dfd94b64ea034b22daccae42
AdamGalla <ado.galla@gmail.com>
2023-05-24 00:11:32 +0200

    Retrieve seen and liked

--- DataAccess/Models/Movie.cs
+++ DataAccess/Models/Movie.cs
@@ -4,4 +4,6 @@ public record Movie
     public int Id { get; set; }
     public string Name { get; set; }
     public string Url { get; set; }
+    public bool? Seen { get; set; }
+    public bool? Liked { get; set; }
 }
